9348c5e0449907bd7284ccfd9d36b67e21cac1e3
Alexey Parfenov <alepar@gmail.com>
2011-05-13 12:20:20 +0400

    moved downspeed/eta to first column

--- client/src/java/main/ru/alepar/vuzetty/client/gui/DownloadStatsDisplayer.java
+++ client/src/java/main/ru/alepar/vuzetty/client/gui/DownloadStatsDisplayer.java
@@ -46,36 +46,36 @@ public class DownloadStatsDisplayer {
         statsPanel.setLayout(new FormLayout("fill:d:noGrow,left:4dlu:noGrow,fill:max(d;4px):noGrow,left:4dlu:noGrow,fill:14px:grow,left:4dlu:noGrow,fill:max(d;4px):noGrow,left:4dlu:noGrow,fill:max(d;4px):noGrow", "center:p:noGrow,top:4dlu:noGrow,center:max(d;4px):noGrow"));
         torrentPanel.add(statsPanel, cc.xy(1, 3));
 
-        statusCaption = new JLabel();
-        statusCaption.setText("Status  ");
-        statsPanel.add(statusCaption, cc.xy(1, 1));
-        statusValue = new JLabel();
-        statusValue.setText(" - ");
-        statsPanel.add(statusValue, cc.xy(3, 1));
+        downloadSpeedCaption = new JLabel();
+        downloadSpeedCaption.setText("Down speed  ");
+        statsPanel.add(downloadSpeedCaption, cc.xy(1, 1));
+        downloadSpeedValue = new JLabel();
+        downloadSpeedValue.setText(" - ");
+        statsPanel.add(downloadSpeedValue, cc.xy(3, 1));
 
-        torrentSizeCaption = new JLabel();
-        torrentSizeCaption.setText("Size  ");
-        statsPanel.add(torrentSizeCaption, cc.xy(1, 3));
-        torrentSizeValue = new JLabel();
-        torrentSizeValue.setText(" - ");
-        statsPanel.add(torrentSizeValue, cc.xy(3, 3));
+        etaCaption = new JLabel();
+        etaCaption.setText("ETA  ");
+        statsPanel.add(etaCaption, cc.xy(1, 3));
+        etaValue = new JLabel();
+        etaValue.setText(" - ");
+        statsPanel.add(etaValue, cc.xy(3, 3));
 
         final Spacer spacer1 = new Spacer();
         statsPanel.add(spacer1, cc.xy(5, 1, CellConstraints.DEFAULT, CellConstraints.FILL));
 
-        downloadSpeedCaption = new JLabel();
-        downloadSpeedCaption.setText("Down speed  ");
-        statsPanel.add(downloadSpeedCaption, cc.xy(7, 1));
-        downloadSpeedValue = new JLabel();
-        downloadSpeedValue.setText(" - ");
-        statsPanel.add(downloadSpeedValue, cc.xy(9, 1));
+        statusCaption = new JLabel();
+        statusCaption.setText("Status  ");
+        statsPanel.add(statusCaption, cc.xy(7, 1));
+        statusValue = new JLabel();
+        statusValue.setText(" - ");
+        statsPanel.add(statusValue, cc.xy(9, 1));
 
-        etaCaption = new JLabel();
-        etaCaption.setText("ETA  ");
-        statsPanel.add(etaCaption, cc.xy(7, 3));
-        etaValue = new JLabel();
-        etaValue.setText(" - ");
-        statsPanel.add(etaValue, cc.xy(9, 3));
+        torrentSizeCaption = new JLabel();
+        torrentSizeCaption.setText("Size  ");
+        statsPanel.add(torrentSizeCaption, cc.xy(7, 3));
+        torrentSizeValue = new JLabel();
+        torrentSizeValue.setText(" - ");
+        statsPanel.add(torrentSizeValue, cc.xy(9, 3));
     }
 
     public JPanel getRootPanel() {
