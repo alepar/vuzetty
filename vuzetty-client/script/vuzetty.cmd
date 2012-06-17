f254f455a067a06d8d74347813781a7fd1a42730
Alexey Parfenov <alepar@gmail.com>
2012-06-17 20:36:44 +0400

    advancing version to 0.2-SNAPSHOT

--- vuzetty-client/script/vuzetty.cmd
+++ vuzetty-client/script/vuzetty.cmd
@@ -1,2 +1,2 @@
 @echo off
-start javaw -jar "c:\Program Files (x86)\vuzetty\vuzetty-client-0.1-jar-with-dependencies.jar" %*
+start javaw -jar "c:\Program Files (x86)\vuzetty\vuzetty-client-0.2-SNAPSHOT-jar-with-dependencies.jar" %*
