73f59e4e9cbdba1269769820f9b35a6b08baf561
Alexey Parfenov <alepar@gmail.com>
2012-06-17 20:44:08 +0400

    #16: renaming vuzetty-client to vuzetty-client-desktop

---
+++ vuzetty-client-desktop/src/java/main/ru/alepar/vuzetty/client/gui/MonitorTorrent.java
@@ -0,0 +1,131 @@
+package ru.alepar.vuzetty.client.gui;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.alepar.vuzetty.api.DownloadStats;
+import ru.alepar.vuzetty.client.jmx.MonitorTorrentMXBean;
+import ru.alepar.vuzetty.client.remote.VuzettyClient;
+import sun.awt.VerticalBagLayout;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MonitorTorrent implements MonitorTorrentMXBean {
+
+    private static final String ICON_PATH = "ru/alepar/vuzetty/client/gui/vuze.png";
+
+    private final Logger log = LoggerFactory.getLogger(MonitorTorrent.class);
+    private final Map<String, DownloadStatsDisplayer> hashes = new HashMap<>();
+
+    private final VuzettyClient client;
+
+    private final JFrame frame;
+    private final JPanel contentPane;
+
+    static {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception ignored) {
+        }
+    }
+
+    public MonitorTorrent(VuzettyClient client) {
+        this.client = client;
+
+        client.setStatsListener(new StatsListener());
+
+        contentPane = new JPanel();
+        contentPane.setLayout(new VerticalBagLayout());
+
+        frame = new JFrame();
+        frame.setTitle("vuzetty @ " + client.getAddress());
+        frame.setContentPane(contentPane);
+        frame.setIconImage(Toolkit.getDefaultToolkit().getImage(MonitorTorrent.class.getClassLoader().getResource(ICON_PATH)));
+        frame.setSize(350, 0);
+        frame.setVisible(true);
+
+        frame.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+
+        new Thread(new StatPoller()).start();
+    }
+
+    @Override
+    public void addTorrent(String argument) {
+        log.info("adding torrent={}", argument);
+        try {
+            if (isLocalFile(argument)) {
+                client.addTorrent(readFile(argument));
+            } else {
+                client.addTorrent(argument);
+            }
+        } catch (IOException e) {
+            log.error("failed to add torrent=" + argument, e);
+        }
+    }
+
+    private static boolean isLocalFile(String argument) {
+        return new File(argument).isFile();
+    }
+
+    @Override
+    public boolean check() {
+        return true;
+    }
+
+    private class StatsListener implements ru.alepar.vuzetty.client.remote.StatsListener {
+        @Override
+        public void onStatsUpdate(DownloadStats[] updatedStats) {
+            for (final DownloadStats stat : updatedStats) {
+                DownloadStatsDisplayer displayer = hashes.get(stat.hash);
+
+                if(displayer == null) {
+                    displayer = new DownloadStatsDisplayer();
+                    contentPane.add(displayer.getRootPanel());
+                    hashes.put(stat.hash, displayer);
+                }
+
+                displayer.updateStats(stat);
+            }
+
+            if (frame.getHeight() != frame.getPreferredSize().getHeight()) {
+                frame.setSize(frame.getWidth(), (int) frame.getPreferredSize().getHeight());
+            }
+        }
+    }
+
+    private class StatPoller implements Runnable {
+
+        @Override
+        public void run() {
+            try {
+                while (true) {
+                    client.pollForStats();
+                    Thread.sleep(1000L);
+                }
+            } catch (InterruptedException ignored) {
+            }
+        }
+    }
+
+    private static byte[] readFile(String arg) throws IOException {
+        try (InputStream is = new FileInputStream(new File(arg))) {
+            final ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            byte buffer[] = new byte[10240];
+            int read;
+            while ((read = is.read(buffer)) != -1) {
+                bos.write(buffer, 0, read);
+            }
+            return bos.toByteArray();
+        }
+    }
+
+}
