7fe9b48253608d83f5aadfd0186dd9fc9ea13256
Alexey Parfenov <alepar@gmail.com>
2012-06-21 12:21:42 +0400

    #11: another IPC method that I will prob use

--- vuzetty-server/src/java/main/ru/alepar/vuzetty/server/api/MediaServerApi.java
+++ vuzetty-server/src/java/main/ru/alepar/vuzetty/server/api/MediaServerApi.java
@@ -1,9 +1,11 @@
 package ru.alepar.vuzetty.server.api;
 
+import org.gudy.azureus2.plugins.disk.DiskManagerFileInfo;
 import org.gudy.azureus2.plugins.download.Download;
 
 public interface MediaServerApi {
 
     String getContentURL(Download d);
+    String getContentURL(DiskManagerFileInfo file);
 
 }
--- vuzetty-server/src/java/main/ru/alepar/vuzetty/server/api/VuzeMediaServerApi.java
+++ vuzetty-server/src/java/main/ru/alepar/vuzetty/server/api/VuzeMediaServerApi.java
@@ -1,18 +1,20 @@
 package ru.alepar.vuzetty.server.api;
 
 import org.gudy.azureus2.plugins.PluginInterface;
+import org.gudy.azureus2.plugins.PluginManager;
+import org.gudy.azureus2.plugins.disk.DiskManagerFileInfo;
 import org.gudy.azureus2.plugins.download.Download;
 import org.gudy.azureus2.plugins.ipc.IPCException;
 import org.gudy.azureus2.plugins.ipc.IPCInterface;
 
 public class VuzeMediaServerApi implements MediaServerApi {
 
-    private final PluginInterface pluginInterface;
+    private final PluginManager pluginManager;
 
     private volatile IPCInterface mediaIpc;
 
-    public VuzeMediaServerApi(PluginInterface pluginInterface) {
-        this.pluginInterface = pluginInterface;
+    public VuzeMediaServerApi(PluginManager pluginManager) {
+        this.pluginManager = pluginManager;
     }
 
     @Override
@@ -24,9 +26,18 @@ public class VuzeMediaServerApi implements MediaServerApi {
         }
     }
 
+    @Override
+    public String getContentURL(DiskManagerFileInfo file) {
+        try {
+            return (String) getMediaIpc().invoke("getContentURL", new Object[]{file});
+        } catch (IPCException e) {
+            throw new RuntimeException("got exception while invoking azupnpav.getContentURL()", e);
+        }
+    }
+
     private IPCInterface getMediaIpc() {
         if (mediaIpc == null) {
-            final PluginInterface mediaInterface = pluginInterface.getPluginManager().getPluginInterfaceByID("azupnpav", true);
+            final PluginInterface mediaInterface = pluginManager.getPluginInterfaceByID("azupnpav", true);
             if(mediaInterface == null) {
                 throw new RuntimeException("couldnot find azupnpav plugin - check it is installed and started up");
             }
