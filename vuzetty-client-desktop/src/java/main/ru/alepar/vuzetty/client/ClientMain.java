4b6a74f758ecab45f1cf273e8c1e259755a5a9a5
Alexey Parfenov <alepar@gmail.com>
2012-07-02 02:13:24 +0400

    fixes #20: handle differences in openjdk / sun jdk javaws

--- vuzetty-client-desktop/src/java/main/ru/alepar/vuzetty/client/ClientMain.java
+++ vuzetty-client-desktop/src/java/main/ru/alepar/vuzetty/client/ClientMain.java
@@ -8,6 +8,8 @@ import ru.alepar.vuzetty.client.jmx.MonitorTorrentMXBean;
 import ru.alepar.vuzetty.client.remote.VuzettyClient;
 
 import java.net.InetSocketAddress;
+import java.util.ArrayList;
+import java.util.List;
 
 import static ru.alepar.vuzetty.client.classload.MonitorLookupLoader.loadLookup;
 
@@ -17,7 +19,9 @@ public class ClientMain {
     private static final InetSocketAddress SERVER_ADDRESS = new InetSocketAddress("azureus.alepar.ru", 31337);
 
     public static void main(String[] args) throws Exception {
+        args = cleanupJwsMess(args);
         Thread.setDefaultUncaughtExceptionHandler(new DefaultThreadExceptionHandler());
+
         try {
             log.debug("looking for existing monitor");
             final MonitorLookup monitorLookup = loadLookup();
@@ -37,6 +41,16 @@ public class ClientMain {
             log.error("caught exception", e);
             System.exit(1);
         }
+    }
+
+    private static String[] cleanupJwsMess(String[] args) {
+        final List<String> newArgs = new ArrayList<String>(args.length);
+        for (String arg : args) {
+            if(!"-open".equals(arg)) {
+                newArgs.add(arg);
+            }
+        }
+        return newArgs.toArray(new String[newArgs.size()]);
     }
 
 }
